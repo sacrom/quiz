a184991fe660325cd129140638a21e67717716e6
Marcos Gutierrez Batz <sacrom@gmail.com>
2015-06-24 18:55:30 +0100

    Mudularizado el codigo de dialogo

--- public/js/dlg.js
+++ public/js/dlg.js
@@ -1,14 +1,16 @@
-$('#confirmDelete').on('show.bs.modal', function (e) {
+$('#confirmDlg').on('show.bs.modal', function (e) {
   $message = $(e.relatedTarget).attr('data-message');
   $(this).find('.modal-body p').text($message);
   $title = $(e.relatedTarget).attr('data-title');
   $(this).find('.modal-title').text($title);
+  $class = $(e.relatedTarget).attr('data-btn-class');
+  $(this).find('.modal-footer #confirm').attr('class', 'btn ' + $class);
 
   var form = $(e.relatedTarget).closest('form');
   $(this).find('.modal-footer #confirm').data('form', form);
 });
 
-$('#confirmDelete').find('.modal-footer #confirm').on('click', function(){
+$('#confirm').find('.modal-footer #confirm').on('click', function(){
   $(this).data('form').submit();
 });
 
